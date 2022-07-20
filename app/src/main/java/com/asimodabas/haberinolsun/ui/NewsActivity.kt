22c83ff46211774689b76cb50ec04e65a0b2a994
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-07-21 01:40:33 +0300

    build gradle update

--- app/src/main/java/com/asimodabas/haberinolsun/ui/NewsActivity.kt
+++ app/src/main/java/com/asimodabas/haberinolsun/ui/NewsActivity.kt
@@ -2,11 +2,26 @@ package com.asimodabas.haberinolsun.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.asimodabas.haberinolsun.R
+import com.asimodabas.haberinolsun.databinding.ActivityNewsBinding
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNewsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+
     }
 }
