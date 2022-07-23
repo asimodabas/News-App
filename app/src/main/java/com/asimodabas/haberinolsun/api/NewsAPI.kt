a49f90eaadb0385a8401bf8e10de10613b018575
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-07-23 16:24:21 +0300

    retrofit added

--- app/src/main/java/com/asimodabas/haberinolsun/api/NewsAPI.kt
+++ app/src/main/java/com/asimodabas/haberinolsun/api/NewsAPI.kt
@@ -1,4 +1,31 @@
 package com.asimodabas.haberinolsun.api
 
+import com.asimodabas.haberinolsun.NewsResponse
+import com.asimodabas.haberinolsun.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NewsAPI {
+
+    @GET("xxxx")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY,
+    ):Response<NewsResponse>
+
+    @GET("xxxx")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery: String,
+        @Query("page")
+        pageNumber: Int = 1,
+        @Query("apiKey")
+        apiKey: String = API_KEY,
+    ):Response<NewsResponse>
+
 }
---
+++ app/src/main/java/com/asimodabas/haberinolsun/api/RetrofitInstance.kt
@@ -0,0 +1,29 @@
+package com.asimodabas.haberinolsun.api
+
+import com.asimodabas.haberinolsun.util.Constants.Companion.BASE_URL
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitInstance {
+    companion object{
+        private val retrofit by lazy {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+                Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build()
+        }
+
+        val api by lazy {
+            retrofit.create(NewsAPI::class.java)
+        }
+    }
+}
