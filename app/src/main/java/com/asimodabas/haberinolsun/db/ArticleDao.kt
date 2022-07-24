59cace19de94e7ad6416efccc018e1ff8efaa227
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-07-24 13:08:07 +0300

    dao and Article Setup Room

---
+++ app/src/main/java/com/asimodabas/haberinolsun/db/ArticleDao.kt
@@ -0,0 +1,19 @@
+package com.asimodabas.haberinolsun.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.asimodabas.haberinolsun.Article
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article) : Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles():LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
+}
