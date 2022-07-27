c86b50931805a57934103eef03ffb8d5304e401c
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-07-28 00:03:10 +0300

    bug fixed

--- app/src/main/java/com/asimodabas/haberinolsun/db/Converters.kt
+++ app/src/main/java/com/asimodabas/haberinolsun/db/Converters.kt
@@ -1,15 +1,17 @@
 package com.asimodabas.haberinolsun.db
 
 import androidx.room.TypeConverter
+import com.asimodabas.haberinolsun.model.Source
 
 class Converters {
+
     @TypeConverter
-    fun fromSource(source: Source):String{
+    fun fromSource(source: Source): String {
         return source.name
     }
 
     @TypeConverter
-    fun toSource(name:String): Source {
-        return Source(name,name)
+    fun toSource(name: String): Source {
+        return Source(name, name)
     }
 }
