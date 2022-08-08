6dbe76dabced911d11716c2fb9083a3ff08f0d2d
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-08-08 20:03:09 +0300

    news repository

--- app/src/main/java/com/asimodabas/haberinolsun/repository/NewsRepository.kt
+++ app/src/main/java/com/asimodabas/haberinolsun/repository/NewsRepository.kt
@@ -2,6 +2,7 @@ package com.asimodabas.haberinolsun.repository
 
 import com.asimodabas.haberinolsun.api.RetrofitInstance
 import com.asimodabas.haberinolsun.db.ArticleDatabase
+import com.asimodabas.haberinolsun.model.Article
 
 class NewsRepository(
     val db: ArticleDatabase
@@ -11,4 +12,10 @@ class NewsRepository(
 
     suspend fun searchNews(searchQuery: String,pageNumber: Int) =
         RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article:Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
 }
